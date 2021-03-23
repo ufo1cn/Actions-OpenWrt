bc2a7a94891288c9af8416f74dfed81c9c3c9ed2
ufo1cn <30489476+ufo1cn@users.noreply.github.com>
2021-03-23 14:13:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# 开启128MB flash（去掉下面代码前面的#生效，本人在网上收集的，没经测试）
+sed -i  s/'23552k(ubi),25600k@0x6c0000(firmware)'/'120832k(ubi),122880k@0x6c0000(firmware)'/ target/linux/ar71xx/image/legacy.mk
